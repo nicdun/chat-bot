38757b6a7c5e3defb7352eb520231dc15e3b7a92
Dominik Ocsofszki <dominik.ocsofszki@senacor.com>
2025-03-20 10:38:52 +0100

    Change Docker-image to asksenacor-client-webapp: and SNAPSHOT, if run on main pipeline Changes to be committed: 	modified:   .github/scripts/build_docker_test_run_delete_later.sh 	modified:   .github/workflows/_subflow_build_stage.yml

--- .github/scripts/build_docker_test_run_delete_later.sh
+++ .github/scripts/build_docker_test_run_delete_later.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+DOCKER_IMAGE="asksenacor-frontend-nextjs"
+DOCKER_TAG="SNAPSHOT"
+DOCKER_FILE="docker/Dockerfile-nextjs"
 
 echo "Building image $DOCKER_IMAGE with tag $DOCKER_TAG from $DOCKER_FILE with args $DOCKER_BUILD_ARGS"
 
@@ -7,7 +10,4 @@ RESOLVED_DOCKER_BUILD_ARGS=`for arg in $(echo $DOCKER_BUILD_ARGS | tr "," " "); 
 
 
 
-DOCKER_IMAGE="asksenacor-frontend-nextjs"
-DOCKER_TAG="SNAPSHOT"
-DOCKER_FILE="docker/Dockerfile-nextjs"
 docker build -t $DOCKER_IMAGE:$DOCKER_TAG -f $DOCKER_FILE $RESOLVED_DOCKER_BUILD_ARGS .
